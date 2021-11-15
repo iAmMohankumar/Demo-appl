872384b33b07597725b98d0666939ee8f5457e3e
Mohan <mohan.muniraju@neudesic.com>
2021-11-15 12:30:39 +0530

    Added required featured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetUpsPage from "./pages/AllMeetups";
+import NewMeetUpPage from "./pages/NewMeetUp";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
-    <div className="App"></div>
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/" exact element={<AllMeetUpsPage />} />
+          <Route path="/new-meetup" element={<NewMeetUpPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Layout>
+    </div>
   );
 }
 
---
+++ src/pages/Favorites.js
@@ -0,0 +1,7 @@
+function FavoritesPage(props) {
+    return <div>
+        <h1>{props.title}</h1>
+    </div>
+}
+
+export default FavoritesPage;
